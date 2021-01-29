45413c003a7dd620d2538246764549fb256b9d3d
antonzaharia <zaharia_anton@yahoo.com>
2021-01-29 17:23:42 +0000

    terminal body created

--- weboject/src/components/Main/Content.js
+++ weboject/src/components/Main/Content.js
@@ -1,5 +1,17 @@
 import React from "react";
+import MinimizeIcon from "@material-ui/icons/Minimize";
+import CloseIcon from "@material-ui/icons/Close";
 
 export default function Content() {
-  return <div className="content"></div>;
+  return (
+    <div className="content">
+      <div className="terminal">
+        <div className="terminal-top-bar">
+          <MinimizeIcon />
+          <CloseIcon />
+        </div>
+        <div className="terminal-body"></div>
+      </div>
+    </div>
+  );
 }
