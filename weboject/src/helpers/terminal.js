ccf1853fe99d100a61e47ab19801256f50fec5e2
antonzaharia <zaharia_anton@yahoo.com>
2021-01-30 12:50:23 +0000

    random letters array generator

--- weboject/src/helpers/terminal.js
+++ weboject/src/helpers/terminal.js
@@ -1,3 +1,14 @@
-export function randomNumber() {
-  return Math.floor(Math.random() * 26) + 1;
+const ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+function randomNumber() {
+  return Math.floor(Math.random() * 26) + 1 - 1;
+}
+
+function createArray() {
+  let array = [];
+  let number = Math.floor(Math.random() * 10) + 1;
+  for (let i = 0; i <= number; i++) {
+    array.push(ABC[randomNumber()]);
+  }
+  return array;
 }
