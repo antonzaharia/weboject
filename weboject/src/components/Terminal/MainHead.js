3777a5a5c34151821a10e9fa6c0b4da09072e971
antonzaharia <zaharia_anton@yahoo.com>
2021-02-02 19:18:09 +0000

    typer effect created

--- weboject/src/components/Terminal/MainHead.js
+++ weboject/src/components/Terminal/MainHead.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
+import gsap from "gsap";
 
 export default class MainHead extends Component {
+  text = "Welcome to Weboject".split("");
+  show = "";
+  count = 0;
+
+  componentDidMount() {
+    setInterval(() => {
+      gsap.to(".typer", { opacity: 0 });
+      if (this.count < this.text.length) {
+        this.show = `${this.show}${this.text[this.count]}`;
+        this.count++;
+      }
+    }, 500);
+    setInterval(() => {
+      gsap.to(".typer", { opacity: 1 });
+    }, 1000);
+  }
   render() {
     return (
       <div className="main-head">
-        <h1>MainHead</h1>
+        <h1>
+          {this.show}
+          <span className="typer">_</span>
+        </h1>
       </div>
     );
   }
